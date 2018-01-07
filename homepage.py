29480fb750065e42ac31865964fc1cf77041afee
YulinLei <lyl@907670433@qq.com>
2018-01-07 14:06:10 +0800

    create a remote repo on Github for my flask-own

--- homepage.py
+++ homepage.py
@@ -1,17 +1,28 @@
-from flask import Flask, render_template
-from flask.ext.bootstrap import Bootstrap
-from flask.ext.script import Manager
-from flask.ext.moment import Moment
+from flask import Flask, render_template, session, redirect, url_for
+from flask_bootstrap import Bootstrap
+from flask_script import Manager
+from flask_moment import Moment
 from datetime import datetime
+from flask_wtf import Form
+from wtforms import StringField, SubmitField
+from wtforms.validators import Required
 
+class NameForm(Form):
+	name = StringField('What is your name?', validators = [Required()])
+	submit = SubmitField('Submit')
+	
 app=Flask(__name__)
 manager=Manager(app)
 bootstrap=Bootstrap(app)
 moment=Moment(app)
-
-@app.route('/')
+app.config['SECRET_KEY'] = '12345'
+@app.route('/', methods = ['GET', 'POST'])
 def index():
-	return render_template('base.html', current_time=datetime.utcnow())
+	form = NameForm()
+	if form.validate_on_submit():
+		session['name'] = form.name.data
+		return redirect(url_for('index'))
+	return render_template('base.html', current_time=datetime.utcnow(), form = form, name = session.get('name'))
 #	return '<h1>hahaha</h1>'
 
 @app.errorhandler(404)
